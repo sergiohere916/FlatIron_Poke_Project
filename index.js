666b833e3bc3ed307568ba38a92011d2124d2171
Sergio Heredia <sergioheredia1996@gmail.com>
2023-08-15 15:15:51 -0700

    JS MVP complete, changes to CSS, changes to HTML

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-
-
 const url = `https://courses.cs.washington.edu/courses/cse154/webservices/pokedex/pokedex.php?pokemon=`;
 const pokeUrl = "http://localhost:3000/pokemon/";
 const pokeTeamUrl = "http://localhost:3000/currentTeam";
+const pokeAllTeamsUrl = "http://localhost:3000/teams"
 
 const searchNameForm = document.getElementById("poke-form");
+const currentTeamForm = document.getElementById("current-team-form")
+const allTeamsForm = document.getElementById("all-teams-form")
+const ratingForm = document.getElementById("rating-form");
+
 const nameH1 = document.getElementById("pokemon-name");
 const entryId = document.getElementById("entry-id");
 const description = document.getElementById("description");
@@ -19,22 +22,32 @@ const speciesSpan = document.getElementById("poke-species");
 const evolutionList = document.getElementById("evolutions-list");
 const addToTeam = document.getElementById("add-poke");
 
+const allTeamsHolder = document.getElementById("team-holder-rater")
+
 //Grabbing elements required for displaying current selected team
-const currentPoke1Name = document.getElementById("poke-1-name");
-const currentPoke1Img = document.getElementById("poke-1-img");
-const currentPoke2Name = document.getElementById("poke-2-name");
-const currentPoke2Img = document.getElementById("poke-2-img");
-const currentPoke3Name = document.getElementById("poke-3-name");
-const currentPoke3Img = document.getElementById("poke-3-img");
-const currentPoke4Name = document.getElementById("poke-4-name");
-const currentPoke4Img = document.getElementById("poke-4-img");
-const currentPoke5Name =document.getElementById("poke-5-name");
-const currentPoke5Img = document.getElementById("poke-5-img");
-const currentPoke6Name = document.getElementById("poke-6-name");
-const currentPoke6Img = document.getElementById("poke-6-img");
+const currentTeamHolder = document.getElementById("current-team-holder");
+const currentTeam = document.getElementById("current-team");
 
 let mainPoke = {};
-let dbPoke = {}
+let dbPoke = {};
+let pokemonObjs = [];
+let selectedTeamObj = {};
+let favorites = [];
+
+let currentTeamObj = {
+    teamName: "",
+    pokemons: [],
+    ratings: {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0
+    },
+    averageRating: 0,
+    favorited: false
+};
+
 
 function renderPokemon(poke, poke2) {
     mainPoke = poke; 
@@ -75,41 +88,282 @@ function displayPokeInfo(pokeName) {
         fetch(`http://localhost:3000/pokemon/${pokeId}`)
         .then(res => res.json())
         .then(poke2 => {
-           renderPokemon(poke, poke2);
+        renderPokemon(poke, poke2);
         })
     })
 }
 
+//This is used to render the main pokemon image and information onto webpage based on what pokemon the user entered in the search
 searchNameForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const userInputtedName = e.target[0].value; 
     displayPokeInfo(userInputtedName);
 })
 
-addToTeam.addEventListener("click", (e) => {
-    if (currentPoke1Name.textContent === "") {
-        currentPoke1Name.textContent = mainPoke.name; 
-        currentPoke1Img.src = dbPoke.sprite; 
-    } else if (currentPoke2Name.textContent === "") {
-        currentPoke2Name.textContent = mainPoke.name; 
-        currentPoke2Img.src = dbPoke.sprite; 
-    } else if (currentPoke3Name.textContent === "") {
-        currentPoke3Name.textContent = mainPoke.name; 
-        currentPoke3Img.src = dbPoke.sprite; 
-    } else if (currentPoke4Name.textContent === "") {
-        currentPoke4Name.textContent = mainPoke.name; 
-        currentPoke4Img.src = dbPoke.sprite; 
-    } else if (currentPoke5Name.textContent === "") {
-        currentPoke5Name.textContent = mainPoke.name; 
-        currentPoke5Img.src = dbPoke.sprite; 
-    } else if (currentPoke6Name.textContent === "") {
-        currentPoke6Name.textContent = mainPoke.name; 
-        currentPoke6Img.src = dbPoke.sprite; 
+
+
+function deletePokeTeam (newDiv, pokeObj) {
+    const index = pokemonObjs.indexOf(pokeObj);
+    const deletedPokemon = pokemonObjs.splice(index, 1);
+    currentTeamObj.pokemons = pokemonObjs;
+    newDiv.remove();
+}
+
+function updatePokeTeamFront (pokeObj) {
+    const newDiv = document.createElement("div");
+    const newH5 = document.createElement("h5");
+    const newImg = document.createElement("img");
+    const newBtn = document.createElement("button");
+
+    newH5.textContent = mainPoke.name;
+    newImg.src = dbPoke.sprite;
+    newBtn.textContent = "X";
+
+    newDiv.append(newH5);
+    newDiv.append(newImg);
+    newDiv.append(newBtn);
+
+    currentTeam.append(newDiv);
+
+    newBtn.addEventListener("click", () => { 
+        deletePokeTeam(newDiv, pokeObj)
+    })
+
+}
+
+function updatePokeTeam () {
+    let pokeObj = {
+        name: mainPoke.name,
+        sprite: dbPoke.sprite
+    }
+    pokemonObjs.push(pokeObj) //Pushes currently displayed pokemon to user's team array labeled pokemonObjs
+    updatePokeTeamFront(pokeObj) //Uses function to create div, fills div with img, name, and button. Small card div is then appended to larger div
+}
+
+
+addToTeam.addEventListener("click", () => {
+    if (pokemonObjs.length < 6) {
+        updatePokeTeam()  
+    } else {
+        console.log("No more space");
     }
 })
 
-// fetch(pokeTeamUrl)
-// .then(res => res.json())
-// .then(team => {
-//     console.log(team);
+function renderingTeamObj(team) {
+    currentTeam.innerHTML = "";
+    
+    const mainDiv = document.createElement("div");
+    mainDiv.addEventListener("mouseover", (e) => {
+        mainDiv.style.boxShadow = "10px 10px 10px 10px black"                                                                                                                                                                                                                                                                                                                                                                                                                        
+    })
+    mainDiv.addEventListener("mouseleave", (e) => {
+        mainDiv.style.boxShadow = ""
+    })
+
+    const infoDiv = document.createElement("div");
+    const pokeDiv = document.createElement("div");
+    const ratingStar = document.createElement("h1");
+    ratingStar.style.color = "yellow";
+    infoDiv.append(ratingStar);
+
+    let fav = team.favorited;
+
+    if (team.favorited) {
+        ratingStar.textContent = "★";
+    } else {
+        ratingStar.textContent = "✰";
+    }
+
+    ratingStar.addEventListener("click", (e) => {
+        fetch(`${pokeAllTeamsUrl}/${team.id}`, {
+            method: "PATCH",
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify({favorited: !(fav)})
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log("THIS IS YOUR DATA EMAN")
+            console.log(data);
+            fav = data.favorited;
+            if (data.favorited) {
+                ratingStar.textContent = "★";
+                // favorites.push(data);
+            } else {
+                ratingStar.textContent = "✰";
+                // const index = favorites.indexOf(data);
+                // favorites.splice(index,1);
+
+            }
+            console.log(favorites);
+        })
+    })
+    mainDiv.className = "team-div";
+
+    const pokeTeamName = document.createElement("h5");
+    const pokeTeamRating = document.createElement("h5");
+    
+    team.pokemons.forEach(pokemon => {
+        const newImg = document.createElement("img");
+        newImg.src = pokemon["sprite"];
+        newImg.classList = "poke-div";
+        pokeDiv.append(newImg); //Grabs pokemons from team in the backend and appends their image to bottom div holder
+    })
+    
+    pokeTeamName.textContent = `Team Name: ${team.teamName}`;
+    pokeTeamRating.textContent = `Avg Rating: ${team.averageRating}`;
+
+    infoDiv.append(pokeTeamName);
+    infoDiv.append(pokeTeamRating);
+    mainDiv.append(infoDiv, pokeDiv);
+    allTeamsHolder.append(mainDiv);
+
+    pokemonObjs = []; //Resets the team array since user's team has now been sent to the backend
+
+    mainDiv.addEventListener("click", () => {
+        selectedTeamObj = {...team, header: pokeTeamRating};
+        console.log(selectedTeamObj);
+    })
+}
+
+ratingForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const rating = e.target[0].value
+    updateRating(rating);
+})
+
+function updateRating(rating) {
+    selectedTeamObj.ratings[rating] += 1
+    const avgRating = getAvgRating();
+
+    const selectedTeamObjId = selectedTeamObj.id;
+
+    fetch(`${pokeAllTeamsUrl}/${selectedTeamObjId}`, {
+        method: "PATCH",
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify({
+            ratings: selectedTeamObj.ratings,
+            averageRating: avgRating
+        })
+    })
+    .then(response => response.json())
+    .then(team => {
+        selectedTeamObj["header"].textContent = `Avg Rating: ${team.averageRating}`;
+    })
+}
+
+function creatingTeamObj(pokeTeamName) {
+    currentTeamObj.teamName = pokeTeamName;
+    currentTeamObj.pokemons = pokemonObjs;
+
+    console.log("This is about to be pushed to the backend:")
+    console.log(currentTeamObj);
+
+    fetch(pokeAllTeamsUrl, {
+        method: "POST",
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify(currentTeamObj)
+    })
+    .then(response => response.json())
+    .then(team => {
+        console.log(team);
+        renderingTeamObj(team);
+    })
+}
+
+function getAvgRating() {
+    let sum = 0;
+    let numSum = 0;
+    const numbers = Object.values(selectedTeamObj.ratings);
+
+    for (let i = 0; i < numbers.length; i++) {
+        numSum += numbers[i];
+    }
+    for (rate in selectedTeamObj.ratings) {
+        sum += rate * selectedTeamObj.ratings[rate];
+    }
+    return (sum / numSum).toFixed(2);
+}
+
+currentTeamForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const pokeTeamName = e.target[0].value;
+    console.log(pokeTeamName)
+    if ((pokemonObjs.length !== 0) && (pokeTeamName !== "")) {
+        creatingTeamObj(pokeTeamName)
+    } else {
+        alert("Need 1 or more pokemon on team or team name")
+    }
+})
+
+fetch(pokeAllTeamsUrl)
+.then(response => response.json())
+.then(teams => {
+    teams.forEach(team => {
+        renderingTeamObj(team);
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+//CODE FOR DELETIONS HERE: 
+
+
+//This first fetch is needed to reset currentTeam in the backend
+// fetch(pokeTeamUrl, {
+//     method: "PATCH",
+//     headers: {"content-type": "application/json"},
+//     body: JSON.stringify({
+//         teamName: "",
+//         pokemons: pokemonObjs
+//     })
 // })
+// .then(response => response.json())
+// .then(data => {
+//     currentTeam.innerHTML = "";
+// })
+
+
+// fetch(pokeTeamUrl, {
+//     method: "PATCH",
+//     headers: {"content-type":"application/json"},
+//     body: JSON.stringify({
+//         teamName: pokeTeamName
+//     })
+// })
+// .then(response => response.json())
+// .then(unusedData => {
+//     fetch(pokeTeamUrl)
+//     .then(response => response.json())
+//     .then(data => {
+//         currentTeamObj = data;
+
+
+//     })
+// })  
+
+// function deletePokeTeam (newDiv, pokeObj) {
+//     const index = pokemonObjs.indexOf(pokeObj);
+    
+//     const deletedPokemon = pokemonObjs.splice(index, 1);
+    
+//     fetch(pokeTeamUrl, {
+//         method: "PATCH",
+//         headers: {"content-type": "application/json"},
+//         body: JSON.stringify({
+//             pokemons: pokemonObjs
+//         })
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         newDiv.remove();
+//     })
+// }
